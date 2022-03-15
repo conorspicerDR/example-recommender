fcdbdc537b9b71885682d6a0207beb2bf8762686
conor.spicer <conor.spicer@datarobot.com>
2022-03-15 18:46:34 +0000

    refactored to accept customer IDs as input

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
 import pandas as pd
+import numpy as np
+from scipy.sparse import coo_matrix, save_npz
+import pickle
 
 base_path = 'data/'
-csv_users = 'customers.csv'
-csv_items = 'articles.csv'
+csv_users = f'{base_path}customers.csv'
+csv_items = f'{base_path}articles.csv'
+csv_train = f'{base_path}transactions_train.csv'
 
 # load users & items data
 dfu = pd.read_csv(csv_users)
@@ -18,3 +22,26 @@ user_map = {u: uidx for uidx, u in user_ids.items()}
 item_map = {i: iidx for iidx, i in item_ids.items()}
 
 del dfu, dfi
+
+if __name__ == '__main__':
+    # create and save complete CSR matrix used to train/ score model
+    # this is only needed as a one off run (per model version)
+    df = pd.read_csv(csv_train, dtype={'article_id': str}, parse_dates=['t_dat'])
+
+    df['user_id'] = df['customer_id'].map(user_map)
+    df['item_id'] = df['article_id'].map(item_map)
+
+    # create sparse matrices
+    row = df['user_id'].values
+    col = df['item_id'].values
+    data = np.ones(df.shape[0])
+    coo = coo_matrix((data, (row, col)), shape=(len(ALL_USERS), len(ALL_ITEMS)))
+    csr = coo.tocsr()
+
+    save_npz("matrix.npz", csr)
+
+    with open('user_map.pkl', 'wb') as f:
+        pickle.dump(user_map, f)
+
+    with open('item_map.pkl', 'wb') as f:
+        pickle.dump(item_map, f)
--- custom.py
+++ custom.py
@@ -1,23 +1,10 @@
 import pandas as pd
 import numpy as np
-from scipy.sparse import coo_matrix
+from scipy.sparse import load_npz
 import pickle
-from config import user_map, item_map, ALL_USERS, ALL_ITEMS, user_ids, item_ids
-
 
 def transform(data):
-    """ Turn a dataframe with customer IDs into a COO sparse users x items matrix"""
-    data['user_id'] = data['customer_id'].map(user_map)
-    data['item_id'] = data['article_id'].map(item_map)
-
-    # create sparse matrices
-    row = data['user_id'].values
-    col = data['item_id'].values
-    data = np.ones(data.shape[0])
-    coo_train = coo_matrix((data, (row, col)), shape=(len(ALL_USERS), len(ALL_ITEMS)))
-    csr_train = coo_train.tocsr()
-
-    return csr_train
+    return data
 
 
 def load_model(code_dir=''):
@@ -26,42 +13,49 @@ def load_model(code_dir=''):
     return model
 
 
-def score_unstructured(data, model, **kwargs):
+def score_unstructured(customers, model, **kwargs):
+    csr = load_npz('matrix.npz')
+    with open('user_map.pkl', 'rb') as f:
+        user_map = pickle.load(f)
+    user_ids = {uidx: u for u, uidx in user_map.items()}
+
+    with open('item_map.pkl', 'rb') as f:
+        item_map = pickle.load(f)
+    item_ids = {idx: i for i, idx in item_map.items()}
+
+    customers['user_id'] = customers['customer_id'].map(user_map)
+    customers_to_score = customers[customers['user_id'].notnull()]['user_id'].unique()
+
     preds = []
-    batch_size = 2000
-    to_generate = np.arange(len(ALL_USERS))
-    for startidx in range(0, len(to_generate), batch_size):
-        batch = to_generate[startidx: startidx + batch_size]
-        ids, scores = model.recommend(batch, data[batch], N=12, filter_already_liked_items=False)
-        for i, userid in enumerate(batch):
-            customer_id = user_ids[userid]
-            user_items = ids[i]
-            article_ids = [item_ids[item_id] for item_id in user_items]
-            preds.append((customer_id, ' '.join(article_ids)))
+    ids, scores = model.recommend(
+        customers_to_score,
+        csr[customers_to_score],
+        N=12,
+        filter_already_liked_items=False
+    )
 
-    predictions = pd.DataFrame(preds, columns=['customer_id', 'prediction'])
+    for i, userid in enumerate(customers_to_score):
+        customer_id = user_ids[userid]
+        user_items = ids[i]
+        article_ids = [item_ids[item_id] for item_id in user_items]
+        preds.append((customer_id, ' '.join(article_ids)))
 
-    return predictions
+    df_preds = pd.DataFrame(preds, columns=['customer_id', 'prediction'])
+
+    # join any rows not scored (unknown ID)
+    return customers[['customer_id']].merge(df_preds, how='left', on=['customer_id'])
 
 
-# # local testing
-# if __name__ == '__main__':
-#     from datetime import datetime
-#
-#     start = datetime.utcnow()
-#     print('Started at', start)
-#
-#     base_path = 'data/'
-#     csv_train = f'{base_path}transactions_train.csv'
-#     transactions_df = pd.read_csv(csv_train, dtype={'article_id': str}, parse_dates=['t_dat'])
-#
-#     prediction_cut = transactions_df['t_dat'].max() - pd.Timedelta(days=30)
-#     transactions_pred = transactions_df[transactions_df['t_dat'] > prediction_cut].copy()
-#     del transactions_df
-#     csr_pred = transform(transactions_pred)
-#
-#     latest_model = load_model()
-#
-#     df_preds = score(csr_pred, latest_model)
-#     df_preds.to_csv('data/local_test.csv', index=False)
-#     print('Total time:', str(datetime.utcnow() - start))
+# local testing
+if __name__ == '__main__':
+    from datetime import datetime
+
+    start = datetime.utcnow()
+    print('Started at', start)
+    customer_sample = pd.read_csv('data/customers.csv').sample(1000)
+
+    latest_model = load_model()
+
+    predictions = score_unstructured(customer_sample, latest_model)
+    predictions.to_csv('data/local_test.csv', index=False)
+    print('Total time:', str(datetime.utcnow() - start))
